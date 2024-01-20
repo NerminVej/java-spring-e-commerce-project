ecc053c7a83061c563d2ee2b1d6bd883a25afd83
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-20 11:11:50 +0100

    created the shoppingcart entity

--- e-commerce-backend/src/main/java/com/example/demo/model/ShoppingCart.java
+++ e-commerce-backend/src/main/java/com/example/demo/model/ShoppingCart.java
@@ -1,4 +1,55 @@
 package com.example.demo.model;
 
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "shopping_cart")
 public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
