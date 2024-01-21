f048468665cabc554b42c0f8c392f7b628eb6da3
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-21 08:59:52 +0100

    created the jwt util class

---
+++ e-commerce-backend/src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.example.demo.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET = "your_secret_key";
+    private static final long EXPIRATION_TIME = 86400000;
+
+    public static String generateToken(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SECRET));
+    }
+
+    public static String validateTokenAndRetrieveSubject(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC512(SECRET)).build();
+            DecodedJWT jwt = verifier.verify(token);
+            return jwt.getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+}
