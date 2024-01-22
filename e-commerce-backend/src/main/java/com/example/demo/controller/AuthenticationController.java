1b9ef252f92d5840a04b4c95468675403c1bd6d6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-22 17:18:37 +0100

    added the jwt functionality

---
+++ e-commerce-backend/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.JwtAuthenticationResponse;
+import com.example.demo.dto.SignInRequest;
+import com.example.demo.dto.SignUpRequest;
+import com.example.demo.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) {
+        return authenticationService.signup(request);
+    }
+
+    @PostMapping("/signin")
+    public JwtAuthenticationResponse signin(@RequestBody SignInRequest request) {
+        return authenticationService.signin(request);
+    }
+}
--- e-commerce-backend/src/main/java/com/example/demo/service/UserService.java
+++ e-commerce-backend/src/main/java/com/example/demo/service/UserService.java
@@ -2,18 +2,35 @@ package com.example.demo.service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
+    /*
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+*/
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return (UserDetails) userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
     }
 
     public List<User> findAll() {
@@ -24,12 +41,20 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    public User save(User user) {
-        return userRepository.save(user);
-    }
 
     public void deleteById(Long id) {
         userRepository.deleteById(id);
+    }
+
+
+
+    public User save(User newUser) {
+        if (newUser.getId() == null) {
+            newUser.setCreatedAt(LocalDateTime.now());
+        }
+
+        newUser.setUpdatedAt(LocalDateTime.now());
+        return userRepository.save(newUser);
     }
 
 }
