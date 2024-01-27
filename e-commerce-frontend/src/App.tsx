89588a6c7a7f5a7830e6fc0a0b23635db2f20da8
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-27 07:57:22 +0100

    âdded the entire signinpage component

--- e-commerce-frontend/src/App.tsx
+++ e-commerce-frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
-import Login from './components/Login';
+import Login from './pages/login/Login';
+import LoginPage from './pages/login/LoginPage';
+import SignInPage from './pages/signin/SignInPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+
       </Routes>
     </Router>
   );
--- e-commerce-frontend/src/pages/signin/Signin.tsx
+++ e-commerce-frontend/src/pages/signin/Signin.tsx
@@ -17,7 +17,7 @@ export default function Signin() {
                 href="#"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
-                start your 14-day free trial
+                log in if you have an account already
               </a>
             </p>
           </div>
@@ -25,6 +25,53 @@ export default function Signin() {
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form className="space-y-6" action="#" method="POST">
+              <div>
+                  <label
+                    
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    First Name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              
+              
+              <div>
+                  <label
+                    
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Last Name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                
                 <div>
                   <label
                     htmlFor="email"
@@ -43,6 +90,7 @@ export default function Signin() {
                     />
                   </div>
                 </div>
+
   
                 <div>
                   <label
@@ -50,6 +98,25 @@ export default function Signin() {
                     className="block text-sm font-medium text-gray-700"
                   >
                     Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Repeated Password
                   </label>
                   <div className="mt-1">
                     <input
@@ -79,14 +146,7 @@ export default function Signin() {
                     </label>
                   </div>
   
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
+                
                 </div>
   
                 <div>
