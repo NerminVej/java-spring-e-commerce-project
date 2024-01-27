a06e3b9edddd42162841e1057d13967cba1479a2
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-27 10:09:19 +0100

    added the Incentive and PromoSection

--- e-commerce-frontend/src/pages/home/HomePage.tsx
+++ e-commerce-frontend/src/pages/home/HomePage.tsx
@@ -1,9 +1,15 @@
+import Hero from "../../components/Hero";
+import Incentive from "../../components/Incentive";
 import Navbar from "../../components/Navbar";
+import PromoSection from "../../components/PromoSection";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <PromoSection />
+      <Incentive />
     </div>
   );
 };
