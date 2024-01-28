8c8a93ce376d767d6676272c8a80d6918694e9ca
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-28 08:40:20 +0100

    added the footer to the productpage

--- e-commerce-frontend/src/components/ProductList.tsx
+++ e-commerce-frontend/src/components/ProductList.tsx
@@ -47,7 +47,7 @@ const products = [
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <a key={product.id} href={product.href} className="group">
-                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <div className=" w-full overflow-hidden rounded-lg bg-gray-200 ">
                   <img
                     src={product.imageSrc}
                     alt={product.imageAlt}
--- e-commerce-frontend/src/pages/product/ProductPage.tsx
+++ e-commerce-frontend/src/pages/product/ProductPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductList from '../../components/ProductList'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
 const ProductPage = () => {
   return (
     <div>
+      <Navbar />
         <ProductList />
+        <Footer />
     </div>
   )
 }
