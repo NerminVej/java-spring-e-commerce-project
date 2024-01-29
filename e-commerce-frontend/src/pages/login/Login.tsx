72e85bca08ffb364b591a3934cbf82478a4d8d03
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-29 17:21:01 +0100

    added the handleLogin function to the login button

--- e-commerce-frontend/src/pages/login/Login.tsx
+++ e-commerce-frontend/src/pages/login/Login.tsx
@@ -107,6 +107,7 @@ export default function Login() {
               <div>
                 <button
                   type="submit"
+                  onClick={handleLogIn}
                   className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Sign in
