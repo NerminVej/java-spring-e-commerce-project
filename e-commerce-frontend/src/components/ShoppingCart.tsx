3c16cec51cbf5dca691370282225de2f2369b83a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-30 17:53:24 +0100

    deleted some comments

--- e-commerce-frontend/src/components/ShoppingCart.tsx
+++ e-commerce-frontend/src/components/ShoppingCart.tsx
@@ -1,19 +1,4 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+
 import { CheckIcon, ClockIcon, QuestionMarkCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 const products = [
