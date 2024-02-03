432b405e03c2c6f60cf137c896a2a191abd0ebcd
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-02-03 10:14:46 +0100

    added the useState hoook

--- e-commerce-frontend/src/pages/signin/Signin.tsx
+++ e-commerce-frontend/src/pages/signin/Signin.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 
 export default function Signin() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+
     return (
       <>
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
