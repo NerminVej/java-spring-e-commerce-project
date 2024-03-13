edec1dbd1419c00445b27d88b6bae9161c5c6246
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-03-13 16:44:20 +0100

    added another header

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -4,6 +4,9 @@ const Test = () => {
     <div>
       <div>
         <h2 className="text-3xl text-white font-bold">Header</h2>
+     <div>
+        <h2 className="text-3xl text-white font-bold">Header</h2>
+     </div>
       </div>
     </div>
   )
