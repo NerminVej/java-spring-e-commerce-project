f04f6549d245422792c31af81deba72d1533ad44
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-04-24 05:39:28 +0200

    added a div container

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -21,6 +21,9 @@ const Test = () => {
            <div>
            <p className="text-gray-600">This is some sample content for section 3.</p>
            </div>
+           <div>
+            
+           </div>
             </div>
           </div>
         </div>
