36cc0cd282a50a72085b3a78a411484b21970260
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-05-12 06:32:33 +0200

    added a p element

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -25,7 +25,7 @@ const Test = () => {
           <p className="text-gray-600">This is some sample content for section 3.</p>
           <div>
           </div>
-        
+          <p className="text-gray-600">This is some sample content for section 3.</p>
           </div>
            </div>
             </div>
