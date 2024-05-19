faaf42fcbd78fb2743acdff28fc02f44c2112f44
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-05-19 05:22:19 +0200

    deleted divs

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -26,14 +26,7 @@ const Test = () => {
 <div>
   <div>
     <div>
-      <div>
-        <div>
-          <div></div>
-          <div>
-            
-          </div>
-        </div>
-      </div>
+    
     </div>
   </div>
 </div>
