7b6445ff3cdcbdd8e8dbd8089f07a536afd39193
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-05-30 05:45:38 +0200

    another section got added

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -24,6 +24,7 @@ const Test = () => {
            <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 3</h3>
            <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 3</h3>
            <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 3</h3>
+           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 3</h3>
 
           </div>
           </div>
