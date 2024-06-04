286152781ad2dacb5748af9a114e823a64f3e23a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-04 05:39:01 +0200

    added a section

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -22,6 +22,7 @@ const Test = () => {
            <div>
            <div>
            <div>
+           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
 
           </div>
           </div>
