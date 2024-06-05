d765cb981cae25252bdc02a83dd206a91fa32ba0
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-05 05:57:22 +0200

    deleted the sections

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -22,14 +22,6 @@ const Test = () => {
            <div>
            <div>
            <div>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-           <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
 
           </div>
           </div>
