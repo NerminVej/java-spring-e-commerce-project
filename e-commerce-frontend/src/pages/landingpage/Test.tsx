f47287d8fe0a4028d395d4b1e3ec40de5ec69c10
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-10 05:46:56 +0200

    added a div

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -24,6 +24,9 @@ const Test = () => {
            <div className="bg-white p-8 shadow-md rounded-lg">
             <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 1</h3>
             <p className="text-gray-600">This is some sample content for section 1.</p>
+            <div>
+              
+            </div>
           </div>
           </div>
           </div>
