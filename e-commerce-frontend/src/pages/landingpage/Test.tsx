05d24916bda39913b5d325698beb6097e448cef5
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-15 22:23:51 +0200

    deleted h2 tag

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -15,7 +15,6 @@ const Test = () => {
             <p className="text-gray-600">This is some sample content for section 2.</p>
           </div>
           <div className="bg-white p-8 shadow-md rounded-lg">
-            <h2></h2>
 
             <div>
       
