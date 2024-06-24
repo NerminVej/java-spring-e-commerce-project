1d13320cd82fecb3b230c471cad42761d90d5a0a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-24 05:31:38 +0200

    added a div

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -16,7 +16,9 @@ const Test = () => {
          <div>
          <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
 <div>
-  
+  <div>
+    
+  </div>
 </div>
          </div>
           </div>
