c5662fc795354692c66b748f6bb98cbfd9ea5644
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-25 05:44:48 +0200

    added

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -15,7 +15,7 @@ const Test = () => {
             <p className="text-gray-600">This is some sample content for section 2.</p>
          <div>
          <h3 className="text-2xl font-bold text-gray-800 mb-4">Section 2</h3>
-
+<h3></h3>
          </div>
           </div>
        
