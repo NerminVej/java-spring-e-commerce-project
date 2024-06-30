9337dd793f215cf2b47cb9ddba0373e0192c160d
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-30 06:30:12 +0200

    a

--- e-commerce-frontend/src/pages/landingpage/Test.tsx
+++ e-commerce-frontend/src/pages/landingpage/Test.tsx
@@ -11,9 +11,7 @@ const Test = () => {
             <p className="text-gray-600">This is some sample content for section 1.</p>
           </div>
  <div></div>
-       <div>
-        
-       </div>
+  
           </div>
           </div>
           </div>
