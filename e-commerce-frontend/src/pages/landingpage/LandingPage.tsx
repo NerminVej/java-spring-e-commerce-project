8e872f8378a64a86c194a97441d1e2ddb2fb03f0
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-07 06:46:30 +0200

    deleted another "#"

--- e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
+++ e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
@@ -303,7 +303,7 @@ export default function LandingPage() {
                   </div>
                   <div className="mt-6">
                     <a
-                      href="#"
+                      href=""
                       className="flex w-full items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
                     >
                       Sign up
