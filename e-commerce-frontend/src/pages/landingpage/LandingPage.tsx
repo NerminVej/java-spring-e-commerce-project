94374ad03f4316cabba2e4da0a2881cd54c99c8c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-09 06:36:03 +0200

    deleted a "#"

--- e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
+++ e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
@@ -347,7 +347,7 @@ export default function LandingPage() {
                 <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <a
-                      href="#"
+                      href=""
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8"
                     >
                       Get started
