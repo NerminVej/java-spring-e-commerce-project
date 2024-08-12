fef6f91552f08f5737eef73fcd9c2cb53df988e1
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-12 07:39:21 +0200

    deleted a p element

--- e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
+++ e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
@@ -362,9 +362,7 @@ export default function LandingPage() {
 
         <div className="bg-gray-100">
           <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-            <p className="text-center text-base font-semibold text-gray-500">
-              Trusted by over 5 very average small businesses
-            </p>
+       
             <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
               <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                 <img className="h-12"  alt="Tuple" />
