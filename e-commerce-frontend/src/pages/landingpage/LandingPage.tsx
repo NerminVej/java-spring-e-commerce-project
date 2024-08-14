5cc5330203fa3314ba3281d291fcf2b358dbb27f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-14 08:38:25 +0200

    deleted an alt

--- e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
+++ e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
@@ -378,7 +378,6 @@ export default function LandingPage() {
               <div className="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
                 <img
                   className="h-12"
-                  alt="Transistor"
                 />
               </div>
               <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
