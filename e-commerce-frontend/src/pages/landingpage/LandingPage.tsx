abf0ed8a2e2bb31751cd996fb5f8d126691503a6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-14 08:38:15 +0200

    deleted an img

--- e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
+++ e-commerce-frontend/src/pages/landingpage/LandingPage.tsx
@@ -384,7 +384,6 @@ export default function LandingPage() {
               <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
                 <img
                   className="h-12"
-                  src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
                   alt="Workcation"
                 />
               </div>
