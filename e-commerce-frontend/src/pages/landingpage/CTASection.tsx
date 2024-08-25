4e54a173871cb6b07576474ca1a74f482953e43c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-25 06:44:47 +0200

    deleted a "#"

--- e-commerce-frontend/src/pages/landingpage/CTASection.tsx
+++ e-commerce-frontend/src/pages/landingpage/CTASection.tsx
@@ -16,7 +16,7 @@ const CTASection = () => {
             Learn more
           </a>
           <a
-            href="#"
+            href=""
             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-50 px-4 py-3 text-base font-medium text-indigo-800 shadow-sm hover:bg-indigo-100"
           >
             Get started Now!
