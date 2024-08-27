d36c7c51f652fa486c90855162f9e6079da83fcc
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-27 05:21:02 +0200

    removed a comment

--- e-commerce-frontend/src/components/PromoSection.tsx
+++ e-commerce-frontend/src/components/PromoSection.tsx
@@ -56,7 +56,6 @@ const testimonials = [
           </div>
         </section>
   
-        {/* Testimonials */}
         <section
           aria-labelledby="testimonial-heading"
           className="relative mx-auto max-w-7xl py-24 px-4 sm:px-6 lg:py-32 lg:px-8"
