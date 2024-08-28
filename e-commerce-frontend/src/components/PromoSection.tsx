b598be558823acc4c8896452dedafc0b90dfd169
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-08-28 05:11:13 +0200

    removed the aria

--- e-commerce-frontend/src/components/PromoSection.tsx
+++ e-commerce-frontend/src/components/PromoSection.tsx
@@ -57,7 +57,6 @@ const testimonials = [
         </section>
   
         <section
-          aria-labelledby="testimonial-heading"
           className="relative mx-auto max-w-7xl py-24 px-4 sm:px-6 lg:py-32 lg:px-8"
         >
           <div className="mx-auto max-w-2xl lg:max-w-none">
