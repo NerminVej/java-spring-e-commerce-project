6ced4866b0793bc2e4c17c72f8d09638091c99c6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-09-04 05:11:16 +0200

    deleted a paragraph

--- e-commerce-frontend/src/components/Footer.tsx
+++ e-commerce-frontend/src/components/Footer.tsx
@@ -127,7 +127,6 @@ const navigation = {
             </div>
           </div>
           <div className="mt-12 border-t border-gray-200 pt-8">
-            <p className="text-base text-gray-400 xl:text-center"></p>
           </div>
         </div>
       </footer>
