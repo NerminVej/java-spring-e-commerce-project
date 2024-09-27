a7ac07e2c17ccea8fe78afb93a485b5cd33f8ed5
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-09-27 05:28:16 +0200

    remove: link

--- e-commerce-frontend/src/components/Hero.tsx
+++ e-commerce-frontend/src/components/Hero.tsx
@@ -36,7 +36,6 @@ export default function Hero() {
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
                           <img
-                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
                      
                             className="h-full w-full object-cover object-center"
                           />
