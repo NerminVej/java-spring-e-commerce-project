c26d80012eacbbdf8008eb8f89b8260859673c59
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2025-01-20 05:54:26 +0100

    remove: div

--- e-commerce-frontend/src/pages/landingpage/CTASection.tsx
+++ e-commerce-frontend/src/pages/landingpage/CTASection.tsx
@@ -15,7 +15,6 @@ const CTASection = () => {
             href=""
             className="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
           >
-            Learn more
           </a>
           <a
             href=""
